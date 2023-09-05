4ec5298a285819e865ff954ee2be7c6837244b60
avitalkalimi <82336185+avitalkalimi@users.noreply.github.com>
2023-09-05 14:32:01 +0300

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -1,10 +1,9 @@
 #We will import the Animal class.
 #we will use some of the methods that already exist in it, to avoid code duplication, because a bird is a type of animal.
 #Some of the methods will be overridden and implemented here according to the entity's need.
-#This is an abstract class. Talking about a general product.
 from hw5.Animal import Animal
 
-
+#This is an abstract class. Talking about a general product.
 class Bird(Animal):
     #The Bird constructor, produces a new Bird according to the parameters it receives
     #It must accept all parameters, and it has conditions
