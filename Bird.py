b1ff2084514726db78a0f57bbaf0a4ed9549fdda
avitalkalimi <82336185+avitalkalimi@users.noreply.github.com>
2023-09-05 12:48:59 +0300

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -6,17 +6,17 @@ class Bird(Animal):
         Animal.__init__(self, nick_name, price, power, type)
         self.fly = False
 
-    # def __ge__(self, other):
-    #     if not isinstance(other, Animal):
-    #         return False
-    #
-    #     if type(other) != Bird:
-    #         if self.fly:
-    #             return True
-    #         else:
-    #             return self >= other
-    #
-    #     if not other.fly:
-    #         return True
-    #
-    #     return self >= other
+     def __ge__(self, other):
+         if not isinstance(other, Animal):
+             return False
+    
+         if type(other) != Bird:
+             if self.fly:
+                 return True
+             else:
+                 return self >= other
+    
+         if not other.fly:
+             return True
+    
+         return self >= other
