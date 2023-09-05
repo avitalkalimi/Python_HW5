be6e3352f81a163cebc31e46bfc8ddc6b647a66b
avitalkalimi <82336185+avitalkalimi@users.noreply.github.com>
2023-09-05 14:25:06 +0300

    Update Bird.py

--- Bird.py
+++ Bird.py
@@ -1,11 +1,19 @@
+#We will import the Animal class.
+#we will use some of the methods that already exist in it, to avoid code duplication, because a bird is a type of animal.
+#Some of the methods will be overridden and implemented here according to the entity's need.
 from hw5.Animal import Animal
 
 
 class Bird(Animal):
+    #The Bird constructor, produces a new Bird according to the parameters it receives
+    #It must accept all parameters, and it has conditions
+    #For some of the parameters we will use the constructor of an animal.
+    #unique features will be realized here
     def __init__(self, nick_name, price, power, type):
         Animal.__init__(self, nick_name, price, power, type)
         self.fly = False
 
+    ##comparison method for birds
      def __ge__(self, other):
          if not isinstance(other, Animal):
              return False
