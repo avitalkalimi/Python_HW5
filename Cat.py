9c925aa9d276c4a9b653dd822d4a61a437f036b3
avitalkalimi <82336185+avitalkalimi@users.noreply.github.com>
2023-09-05 14:44:27 +0300

    Update Cat.py

--- Cat.py
+++ Cat.py
@@ -1,9 +1,18 @@
+#We will import the Mammal class, which inheritor the Animal class.
+#we will use some of the methods that already exist in it, to avoid code duplication, because a Cat is a type of Mammal.
+#Some of the methods will be overridden and implemented here according to the entity's need.
+#This is an abstract class. Talking about a general product.
 from hw5.Mammal import Mammal
 
 class Cat(Mammal):
+    #The Cat constructor, produces a new Cat according to the parameters it receives
+    #It must accept all parameters, and it has conditions
+    #For some of the parameters we will use the constructor of an Mammal.
+    #unique features will be realized here
     def __init__(self, nick_name, price, power, type="Cat"):
         Mammal.__init__(self, nick_name, price, power, type="Cat")
 
+    #This method overrides the speak method of mammals, and implements it according to the cat's voice
     def speak(self):
         return Mammal.speak(self) + " meow"
 
